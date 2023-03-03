3a6880ef9e63a99910e2d357240ff99896d3ecf6
benvarian <88700858+benvarian@users.noreply.github.com>
2023-03-03 20:33:35 +0800

    base setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
-export default {
+export default defineNuxtConfig( {
   nitro: {
     preset: 'vercel-edge',
   },
-};
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  },
+);

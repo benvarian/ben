ce678810a611e4f77f7e7a4878af0ed1685e62c1
benvarian <88700858+benvarian@users.noreply.github.com>
2023-03-04 21:05:37 +0800

    fix loading

--- components/repo.vue
+++ components/repo.vue
@@ -1,29 +1,33 @@
 <template>
   <div v-for="repo in data" :key="data.id">
     <div v-if="Object.keys(repo.languages).length">
-    <div
-      class="flex-1 px-2 py-2 bg-white border rounded-lg shadow-md hover:bg-gray-100"
-    >
-      <p
-        class="px-4 py-2 text-2xl font-bold font-mono tracking-tight text-gray-900"
+      <div
+        class="flex-1 px-2 py-2 bg-white border rounded-lg shadow-md hover:bg-gray-100"
       >
-        {{ repo.name }}
-      </p>
-      <p class="font-light px-4 py-2 text-blue-400 hover:underline">
-        <NuxtLink :to="repo.url">{{ repo.url }}</NuxtLink>
-      </p>
-      <div>
-        <p class="px-4 py-1">Languages used:</p>
-        <div v-for="(amount, language) in repo.languages">
-          <p class="px-4 py-1">{{ language }}</p>
+        <p
+          class="px-4 py-2 text-2xl font-bold font-mono tracking-tight text-gray-900"
+        >
+          {{ repo.name }}
+        </p>
+        <p class="font-light px-4 py-2 text-blue-400 hover:underline">
+          <NuxtLink :to="repo.url">{{ repo.url }}</NuxtLink>
+        </p>
+        <div>
+          <p class="px-4 py-1">Languages used:</p>
+          <div v-for="(amount, language) in repo.languages">
+            <p class="px-4 py-1">{{ language }}</p>
+          </div>
         </div>
       </div>
-    </div> 
+    </div>
   </div>
-</div>
 </template>
 
 <script setup>
+import { useHead } from "#imports";
+useHead({
+  title: "Development",
+});
 defineProps({
   data: {
     type: Object,
--- pages/development.vue
+++ pages/development.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center py-10">
+  <div v-if="!obj.loading" class="flex justify-center py-10">
     <div class="grid gap-4">
       <repo :data="store.repos" />
     </div>
@@ -7,14 +7,18 @@
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, reactive } from "vue";
 import { useUserStore } from "~~/stores/user";
 const store = useUserStore();
+const obj = reactive({
+  loading: true,
+});
 onMounted(async () => {
   try {
     await store.getRepos();
   } catch (error) {
     console.log(error);
   }
+  obj.loading = false;
 });
 </script>
